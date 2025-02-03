e80ae972c1b3c77523d84893420c9649a2c9bce1
ken123-pixel <planetprogrammer1@gmail.com>
2025-02-03 16:47:28 +0300

    first commit

---
+++ variables.py
@@ -0,0 +1,61 @@
+print("hello world")
+print("hello world")
+print("hello world")
+print(12345)
+print(123.35)
+print("Ken")
+
+# variables
+first_name = "Ken"
+print(first_name)
+
+age = 24
+print(age)
+
+weight =48.5
+print(weight)
+
+# data types
+# integer
+a= 45
+print(a)
+# string
+b = "click to save the form"
+d = "12345"
+# float
+e = 12312.234
+# boolean
+is_on = True
+is_off = False
+
+# input from user
+name = input("What is your name? ")
+print(name)
+
+age= input("How old are you? ")
+print(age)
+
+weight = input("How much do you weigh? ")
+print(weight)
+
+first_number = int(input("What is your first number? "))
+print(first_number)
+
+second_number = int(input("What is your second number? "))
+print(second_number)
+
+summation = first_number + second_number
+print(summation)
+
+weight=float(input("what is your weight?"))
+height=float(input("what is your height?"))
+BMI=weight / (height*height)
+print(BMI)
+
+
+name=input("what is your name?")
+age=int(input("how old are you?"))
+subtract=100 -float(age)
+# year=2025
+ans=2025 -float(subtract)
+print(ans)
